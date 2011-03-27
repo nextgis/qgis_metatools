9e5a505c6da2363a41e204ccaf071cd4a19bea55
(no author) <(no author)@cd73bfe1-043d-e011-9159-0015174e7828>
2011-03-27 12:04:57 +0000

    show available profiles in settings dialog

--- src/metatoolssettings.py
+++ src/metatoolssettings.py
@@ -21,7 +21,12 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+import os
+
 from ui_settings import Ui_MetatoolsSettingsDialog
+
+currentPath = os.path.abspath( os.path.dirname( __file__ ) )
 
 class MetatoolsSettings(QDialog):
     def __init__(self):
@@ -29,14 +34,30 @@ class MetatoolsSettings(QDialog):
         # Set up the user interface from Designer.
         self.ui = Ui_MetatoolsSettingsDialog()
         self.ui.setupUi(self)
-        
+
+        self.manageGui()
+
         self.readSettings()
-        
+
         self.connect(self.ui.btnSelectFilter, SIGNAL("clicked()"), self.updateFilter)
+
+    def manageGui( self ):
+        # populate profiles combobox
+        profilesDir = QDir( QDir.toNativeSeparators( os.path.join( currentPath, "xml_profiles" ) ) )
+        profilesDir.setFilter( QDir.Files | QDir.NoSymLinks | QDir.NoDotAndDotDot )
+        fileFilter = QStringList() << "*.xml" << "*.XML"
+        profilesDir.setNameFilters( fileFilter )
+        profiles = profilesDir.entryList()
+        self.ui.defaultProfileComboBox.addItems( profiles )
 
     def readSettings( self ):
         settings = QSettings( "NextGIS", "metatools" )
         self.ui.leFilterFileName.setText( settings.value( "general/filterFile", QVariant() ).toString() )
+
+        # restore default profile
+        profile = settings.value( "iso19115/defaultProfile", QVariant() ).toString()
+        self.ui.defaultProfileComboBox.setCurrentIndex( self.ui.defaultProfileComboBox.findText( profile ) )
+
 
     def updateFilter( self ):
         fileName = QFileDialog.getOpenFileName( self, self.tr( 'Select filter' ), '.', self.tr( 'Text files (*.txt *.TXT)' ) )
@@ -45,8 +66,13 @@ class MetatoolsSettings(QDialog):
             return
 
         self.ui.leFilterFileName.setText( fileName )
-    
+
     def accept( self ):
+        # save settings
         settings = QSettings( "NextGIS", "metatools" )
         settings.setValue( "general/filterFile", self.ui.leFilterFileName.text() )
+
+        settings.setValue( "iso19115/defaultProfile",  self.ui.defaultProfileComboBox.currentText()  )
+
+        # close dialog
         QDialog.accept( self )
