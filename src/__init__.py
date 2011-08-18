1aaa2f65999942f9b73f8228a1571d3c3dc1f547
(no author) <(no author)@cd73bfe1-043d-e011-9159-0015174e7828>
2011-08-18 13:17:02 +0000

    yet another dummy commit

--- src/__init__.py
+++ src/__init__.py
@@ -25,6 +25,7 @@ mVersion = "0.1.0"
 # to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 # MA 02111-1307, USA.
 #
+#
 #******************************************************************************
 
 def name():
